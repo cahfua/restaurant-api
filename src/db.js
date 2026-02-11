95b346949f69beeb20482328e174072852b9ec2d
cahfua <ahfuacelestine@gmail.com>
2026-02-11 16:25:04 -0700

    Restore src folder and stabilize DB connection

--- src/db.js
+++ src/db.js
@@ -2,15 +2,17 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 
+let client;
 let db;
 
 export async function connectDB() {
   if (db) return db;
 
-  const client = new MongoClient(process.env.MONGO_URI);
+  client = new MongoClient(process.env.MONGO_URI);
   await client.connect();
-  db = client.db("restaurantDB");
 
+  db = client.db("restaurantDB");
   console.log("Connected to MongoDB");
+
   return db;
 }
